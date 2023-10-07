27432f3c33f47e01fff58d4f5765c77bc8be617d
ThatAdwaithGuy <106865321+ThatAdwaithGuy@users.noreply.github.com>
2023-10-07 13:38:57 +0530

    intial commit

---
+++ UIBoz.py
@@ -0,0 +1,40 @@
+from DrawBoz import DrawBoz
+
+'''
+psedo-code
+
+a = DrawBoz([DrawBoz.AddText(f"Heelo {MyDynamicValue}")])
+
+
+
+
+'''
+
+
+
+
+
+
+def PageCounter(PageDict):
+    if not PageDict:
+        latest_key = 1
+    else:
+        # Find the latest key by converting keys to integers and taking the max
+        latest_key = max(map(int, PageDict.keys()))
+        # Increment the latest key by 1
+        latest_key += 1
+    return latest_key
+
+PageDict = {}
+
+class Page:
+    def __init__(self, PageContent: DrawBoz, PageID: int=PageCounter(PageDict), PageName: str='Main Page') -> None:
+        PageDict[PageID] = PageName
+        self.Page = Page
+        self.RenderedPage = Page.RenderString()
+
+    def Refresh(self):
+        print('\033c')
+
+    def Print(self):
+        print(self.RenderedPage)
