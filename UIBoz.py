9c1c382de77bdfd205574e9a27a5dd5e7a0f4172
Adwaith <adwaith.3110@gmail.com>
2023-10-15 18:42:36 +0530

    Why did I take so long to fix this issue.

--- UIBoz.py
+++ UIBoz.py
@@ -1,4 +1,5 @@
-from DrawBoz import DrawBoz, TextInstance, BozInstance
+#from DrawBoz import DrawBoz, TextInstance, BozInstance
+
 from typing import Any, Dict
 
 
@@ -29,10 +30,8 @@ Here all of these share a command var, so a if that the value changes then all o
 
 class Value:
     def __init__(self,
-                 _Value,
+                 _Value: Any,
                  parent_value=None) -> None:
-        
-        
         
         if parent_value is not None and not isinstance(parent_value, Value):
             raise TypeError('Parent Value is not a Value type')
@@ -41,35 +40,15 @@ class Value:
         self.value_buffer = _Value
         #self.self_changed = False
         self.child_values: list[Value] = []
+        self.is_child = False
 
         if parent_value != None:
-            self.is_parent_changed = False
+            self.is_child = True
+            self.changed = False
             self.parent_value = parent_value
             self._Value = parent_value
             self.parent_value.add_child(self)
 
-    ''' For debug purposes ONLY '''
-    
-    def dump_values(self):
-        if isinstance(self._Value, Value):
-            print(f"    Value: {self._Value.get_value()}") 
-            print('\n')
-            print(f"    Value Buffer: {self.value_buffer.get_value()}") 
-            print('\n')
-            print(f"    child values: {self.child_values}")
-            print('\n')
-            print(self.parent_value)
-            print('\n')
-        else:
-            print(f" Value: {self._Value}") 
-            print('\n')
-            print(f" Value Buffer: {self.value_buffer}") 
-            print('\n')
-            print(f" child values: {self.child_values}")
-            print('\n')
-            print(self.parent_value)
-            print('\n')
-    
     ''' <Setters and Getters>'''
 
     def get_value(self):
@@ -78,13 +57,13 @@ class Value:
         else:
             return self._Value
     
-    def set_value(self, new_value):
+    def set_value(self, new_value: Any):
         #if not isinstance(new_value, Value):
         #    raise TypeError('Given Value is not a Value type')
         self.value_buffer = new_value
-        self.is_parent_changed = True
+        self.changed = True
         
-    
+    ''' child_value is Value Type '''
     def add_child(self, child_value):
         if not isinstance(child_value, Value):
             raise TypeError('Given Value is not a Value type')
@@ -94,7 +73,7 @@ class Value:
 
     ''' this should be in the lifetime loop '''
     def check_self_or_parent_is_changed(self) -> bool:
-        if self.is_parent_changed:
+        if self.changed:
             return True
         else:
             return False
@@ -102,7 +81,7 @@ class Value:
     def send_new_value_to_children(self, _Value):
         for i in self.child_values:
             i.parent_value = _Value
-            i.is_parent_changed = True
+            i.changed = True
 
 
 
@@ -117,12 +96,17 @@ class Lifetime:
     
     def refresh_values(self):
         for index, value in self.value_dict.items():
-            if value.check_self_or_parent_is_changed:
-                value._Value = value.value_buffer
-                value.send_new_value_to_children(value._Value)
+            if value.check_self_or_parent_is_changed():
+                if value.is_child:
+                    value.send_new_value_to_children(value.value_buffer)
+                    value._Value = value.parent_value
+                else:
+                    value.send_new_value_to_children(value.value_buffer)
+                    value._Value = value.value_buffer
+                
             
 
-
+'''
 class Page:
     def __init__(self, page_data: BozInstance) -> None:
         self.page_data = page_data
@@ -136,8 +120,8 @@ class Page:
         return self.page_data_rendered.RenderString()
 
     def clear(self) -> None:
-        print('\033c')
-
+        print('c')
+'''
 
 a: Value = Value('hello')
 b: Value = Value('', a)
@@ -150,10 +134,13 @@ life.add_value(a)
 life.add_value(b)
 
 a.set_value('hi')
-
 life.refresh_values()
-
 print(f"A: {a.get_value()}")
+
 print(f"B: {b.get_value()}")
-b.get_value()
+    
+
+
+
+
 
