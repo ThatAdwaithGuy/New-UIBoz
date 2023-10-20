47c37d7fdb28ff4a67b3162590d07a236106ac99
Adwaith <adwaith.3110@gmail.com>
2023-10-20 18:42:20 +0530

    GODBLESS DEBUGGING

--- UIBoz.py
+++ UIBoz.py
@@ -1,9 +1,8 @@
-import os
 from rDrawBoz import DrawBoz, TextInstance, BozInstance
 import time
 from typing import Any, Dict
-import sys
-import keyboard
+from typing import Optional
+
 '''
 =================================MY IDEA==========================
                             A
@@ -28,8 +27,7 @@ Here all of these share a command var, so a if that the value changes then all o
 class Value:
     def __init__(self,
                  _Value: Any,
-                 parent_value: 'Value'=None,
-                 
+                 parent_value: Optional['Value'] | None=None,      
                  is_parent_value_same_as_Value: bool=False) -> None:
         
 
@@ -42,16 +40,24 @@ class Value:
         self._Value: type(_Value) = _Value
         self.value_buffer: type(_Value) = _Value
         self.child_values: list[Value] = []
-        self.changed = False
-        self.is_child = False
-        self.is_same = False
+        self.changed: bool = False
+        self.is_same: bool = False
+        self.parent_value: Optional[Value] = None
 
         if parent_value != None:
             self.parent_value = parent_value
             if is_parent_value_same_as_Value:
                 self.is_same = True
                 self._Value = parent_value
-            self.parent_value.add_child(self)
+            self.parent_value.add_child(Value(self._Value))
+    
+    def take_a_potty(self):
+
+        print(self._Value)
+        
+        print(self.parent_value if self.parent_value == None else self.parent_value.get_value())
+  
+  
 
     ''' <Setters and Getters>'''
 
@@ -62,13 +68,11 @@ class Value:
             return self._Value
     
     def set_value(self, new_value: Any):
-        
         self.value_buffer = new_value
         self.changed = True
         
     ''' child_value is Value Type '''
     def add_child(self, child_value: 'Value'):
-        
         self.child_values.append(child_value)
 
     '''         </>          '''
@@ -80,10 +84,10 @@ class Value:
         else:
             return False
 
-    def send_new_value_to_children(self, _Value: 'Value'):
+    def send_new_value_to_children(self, value: 'Value'):
         for i in self.child_values:
             #print(f'ummm, this is gonna  be bad but lol, Value: {i.get_value()}')
-            i.parent_value = _Value
+            i.parent_value = value
             i.changed = True
 
 
@@ -104,6 +108,8 @@ class Value:
 '''
 
 
+    
+
 class Lifetime:
     def __init__(self, Lifetime_name: str='Main') -> None:
         self.Running = True
@@ -116,16 +122,15 @@ class Lifetime:
     
     def refresh_values(self):
         for index, value in self.value_dict.items():
-            if not value.check_self_or_parent_is_changed():
-                continue
-
-            if value.is_same:
-                value.send_new_value_to_children(value.value_buffer)
-                value._Value = value.parent_value
-            else:
+            if value.changed:
                 value.send_new_value_to_children(value.value_buffer)
                 value._Value = value.value_buffer
+                value.changed = False
 
+                
+
+                
+        
 class Page:
     def __init__(self, page_data: BozInstance) -> None:
         self.page_data = page_data
@@ -142,71 +147,56 @@ class Page:
         print('\033c')
 
 
-class InputFunctions(type):
-    def __new__(cls, name, bases, attrs):
-
-        if "_ready" in attrs and "_update" in attrs:
-            
-            def run(self):
-                self._ready()
-                self.is_running = True
-                while self.is_running:
-                    self._update()
-
-            
-            attrs["run"] = run
-
-       
-        return super().__new__(cls, name, bases, attrs)
 
 
-class MyFunctions(metaclass=InputFunctions):
+
+
+a = Value('Hallo, im Dumb')
+b = Value([], a)
+c = Value([], b)
+d = Value([], c)
+
+life = Lifetime() 
+
+life.add_value(a)
+life.add_value(b)
+life.add_value(c)
+life.add_value(d)
+
+life.refresh_values()
+life.refresh_values()
+life.refresh_values()
+
+for i in range(10):
+
+
+    #print(b.parent_value.get_value())
+    b.set_value(TextInstance(b.parent_value.get_value()).generate_list())
+
+    life.refresh_values()
+    life.refresh_values()
+    life.refresh_values()
+
+    #print(c.parent_value)   
+    c.set_value(BozInstance([DrawBoz.AddText(c.parent_value.get_value())]).generate_list())
+
+    life.refresh_values()
+    life.refresh_values()
+    life.refresh_values()
+
+    #print(d.parent_value)
+
+    d.set_value(DrawBoz(d.parent_value.get_value()))
+
+    life.refresh_values()
+    life.refresh_values()
+    life.refresh_values()
+
+    a.set_value("Hello, I'm smart")
+
+    life.refresh_values()
+    life.refresh_values()
+    life.refresh_values()
+
     
-    def _ready(self):
-        pass
-
-    def _update(self):
-        def keyy(e):
-            return e.name
-        
-        sys.stdout.write("\033[?25l")
-
-        a = Value('Hello')
-        b = Value([], a)
-        c = Value([], b)
-        d = Value([], c)
-        life = Lifetime() 
-        life.add_value(a)
-        life.add_value(b)
-        life.add_value(c)
-        life.add_value(d)
-        
-        
-        b.set_value(TextInstance(b.parent_value.get_value()).generate_list())
-        
-        life.refresh_values()
-
-        c.set_value(BozInstance([DrawBoz.AddText(c.parent_value)]).generate_list())
-
-        life.refresh_values()
-
-        d.set_value(DrawBoz(d.parent_value))
-
-        life.refresh_values()
-      
-        buffer = d.get_value().RenderString()
-        
-        keyboard.on_press(lambda e: a.set_value(e.name))
-
-        sys.stdout.write(buffer)
-        sys.stdout.flush()
-        
-        time.sleep(0.089)
-
-        sys.stdout.write('\033c')
-        sys.stdout.flush()
-        print('\u200B')
-
-
-a = MyFunctions()
-a.run()
+    
